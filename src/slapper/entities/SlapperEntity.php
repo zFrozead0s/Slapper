769596173852d84c75dd133e3fef829199eff002
Duo Incure <duoincuredev@gmail.com>
2022-10-05 18:24:52 +0100

    Stop code continuing and throwing an error even if tag is set properly

--- src/slapper/entities/SlapperEntity.php
+++ src/slapper/entities/SlapperEntity.php
@@ -140,6 +140,7 @@ class SlapperEntity extends Entity implements SlapperInterface{
                 throw new \TypeError('Typed property ' . get_class($this) . "::\$namedtag must be " . CompoundTag::class . ", " . gettype($value) . "used");
             }
             $this->namedTagHack = $value;
+            return;
         }
         throw new \ErrorException("Undefined property: " . get_class($this) . "::\$" . $name);
     }
--- src/slapper/entities/SlapperHuman.php
+++ src/slapper/entities/SlapperHuman.php
@@ -106,6 +106,7 @@ class SlapperHuman extends Human implements SlapperInterface{
                 throw new \TypeError('Typed property ' . get_class($this) . "::\$namedtag must be " . CompoundTag::class . ', ' . gettype($value) . 'used');
             }
             $this->namedTagHack = $value;
+            return;
         }
         throw new \ErrorException('Undefined property: ' . get_class($this) . "::\$" . $name);
     }
